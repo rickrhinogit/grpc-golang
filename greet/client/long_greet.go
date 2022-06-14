1f20dc9aedc84d016783107986690395a1ae5ebd
Rick "The Rhino <32437214+rickrhinogit@users.noreply.github.com>
2022-06-14 16:41:31 -0400

    Uploaded gRPC files
    Code work for gRPC development

---
+++ greet/client/long_greet.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/Clement-Jean/grpc-go-course/greet/proto"
+)
+
+func doLongGreet(c pb.GreetServiceClient) {
+	log.Println("doLongGreet was invoked")
+
+	reqs := []*pb.GreetRequest{
+		{FirstName: "Clement"},
+		{FirstName: "Marie"},
+		{FirstName: "Rhino"},
+	}
+
+	stream, err := c.LongGreet(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error whle calling LongGreet %v\n", err)
+	}
+
+	for _, req := range reqs {
+		log.Printf("Sending req: %v\n", req)
+		stream.Send(req)
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalf("Error while receiving response from LongGreet: %v\n", err)
+	}
+
+	log.Printf("LongGreet: %s\n", res.Result)
+}
